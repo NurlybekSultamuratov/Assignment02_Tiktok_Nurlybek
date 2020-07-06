fdfa040d1db40ceb1f51b5256c23525b5dc894e5
Nurlybek Sultamuratov <nsultamu@my.centennialcollege.ca>
2020-07-05 19:11:06 -0500

    Add project files.

---
+++ Assignment02_Tiktok_Nurlybek/TikTok.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_Tiktok_Nurlybek
+{
+
+    enum Audience { World, Group, Special }
+    class TikTok
+    {
+        //FIELDS//
+        private static int _ID;
+
+        //PROPERTIES//
+        public string Originator { get; }
+        public int Length { get; }
+        public string HashTag { get; }
+        public Audience Audience { get; }
+        public string Id { get; }
+
+        //CONSTRUCTORS//
+
+        public TikTok(string originator, int length, string hashTag, Audience audience)
+        {
+           
+            Originator = originator;
+            Length = length;
+            HashTag = hashTag;
+            Audience = audience;
+
+        }
+
+        private TikTok(string id, string originator, int length, string hashTag, Audience audience)
+        {
+            Id = id;
+            Originator = originator;
+            Length = length;
+            HashTag = hashTag;
+            Audience = audience;
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+        public static TikTok Parse(string line)
+        {
+           line.Split('\t');
+        }
+    }
+}
---
+++ Assignment02_Tiktok_Nurlybek/TikTokManager.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_Tiktok_Nurlybek
+{
+    static class TikTokManager
+    {
+        //FIELDS//
+
+        private static List<TikTok> TIKTOKS;
+
+        private static string Filename;
+
+        //CONSTRUCTOR//
+
+        static TikTokManager()
+        {
+            TIKTOKS = new List<TikTok>();
+
+            TextReader reader = new StreamReader(Filename);
+
+            string line0 = reader.ReadLine();
+            while(line0 != null)
+            {
+                line0 = reader.ReadLine();
+                TikTok.Parse(line0);
+              
+            }
+            reader.Close();
+        }
+
+        //METHODS//
+
+        public static void Initialize()
+        {
+
+        }
+        public static void Show()
+        {
+
+        }
+        public static void Show(string hashtag)
+        {
+
+        }
+        public static void Show(int length)
+        {
+
+        }
+        public static void Show(Audience audience)
+        {
+
+        }
+    }
+}
